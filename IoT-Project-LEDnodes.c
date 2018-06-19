b0a06a320657e625016ec4b93c3fd21e277e3bd4
Arnab Dey <32412133+arnabdeypolimi@users.noreply.github.com>
2018-06-19 20:25:52 +0200

    final

--- IoT-Project-LEDnodes.c
+++ IoT-Project-LEDnodes.c
@@ -1,10 +1,7 @@
 #include "contiki.h"
-
 #include "net/rime.h"
 #include "net/rime/mesh.h"
-
 #include "dev/leds.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -44,30 +41,31 @@ recv(struct unicast_conn *c, const rimeaddr_t *from, uint8_t hops)
   MSG = atoi(message_receive);
   rimeaddr_t addr;
 
-  if(MSG == rimeaddr_node_addr.u8[0]){
+  if(MSG == rimeaddr_node_addr.u8[0]){ /*cheching if this node is destination */
 	leds_on(LEDS_RED);
 	printf("LED Node #%d ON!\n", MSG);
   }
-  else if(MSG > rimeaddr_node_addr.u8[0]){
+  else if(MSG > rimeaddr_node_addr.u8[0]){ /*checking if this message is for higher node*/
 	packetbuf_copyfrom(message_receive, strlen(message_receive));
 	addr.u8[0] = rimeaddr_node_addr.u8[0] + 1;
     	addr.u8[1] = 0;
-   	if(addr.u8[0] != from->u8[0]){
+   	if(addr.u8[0] != from->u8[0]){/*checking if the node address is the same with source node or not*/
 	   unicast_send(c, &addr);
 	   printf("Forwarding packet to node %d ...\n", addr.u8[0]);
    	}
   }
-  else if(MSG == 0){
+  else if(MSG == 0){ /* checking for turn off led message*/
 	leds_off(LEDS_RED);
 	if(rimeaddr_node_addr.u8[0] != 4 && rimeaddr_node_addr.u8[0] != 7 
 					&& rimeaddr_node_addr.u8[0] != 10){
 		addr.u8[0] = rimeaddr_node_addr.u8[0] + 1;
     		addr.u8[1] = 0;
 		unicast_send(c, &addr);
+		printf("Forwarding packet to node %d ...\n", addr.u8[0]);
 		
 	}
   }
-  else{
+  else{ /*if the message for lower node*/
       	packetbuf_copyfrom(message_receive, strlen(message_receive));
 	addr.u8[0] = rimeaddr_node_addr.u8[0] - 1;
 	addr.u8[1] = 0;
